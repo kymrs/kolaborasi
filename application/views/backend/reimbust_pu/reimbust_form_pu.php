283830419d2a5ff9a090314a921cb186b0709bba
kymrs <muhamadrizkysaputra34@gmail.com>
2024-09-12 13:53:57 +0700

    push

--- application/views/backend/reimbust_pu/reimbust_form_pu.php
+++ application/views/backend/reimbust_pu/reimbust_form_pu.php
@@ -305,8 +305,8 @@
 <script>
     $('#tgl_pengajuan').datepicker({
         dateFormat: 'dd-mm-yy',
-        // minDate: new Date(),
-        // maxDate: new Date(),
+        minDate: new Date(),
+        maxDate: new Date(),
 
         // MENGENERATE KODE PREPAYMENT SETELAH PILIH TANGGAL
         onSelect: function(dateText) {
