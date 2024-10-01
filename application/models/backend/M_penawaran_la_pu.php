9727988fd411a018dfb802ea985b5a7c158ae67b
Aruudo09 <audricafabiano@gmail.com>
2024-10-01 11:16:36 +0700

    push land_arrangement

---
+++ application/models/backend/M_penawaran_la_pu.php
@@ -0,0 +1,153 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class M_penawaran_la_pu extends CI_Model
+{
+    // INISIASI VARIABLE
+    var $id = 'id';
+    var $table = 'tbl_land_arrangement';
+    var $column_order = array(null, null, 'no_pelayanan', 'no_arsip', 'produk', 'tgl_berlaku', 'keberangkatan', 'durasi', 'tempat', 'biaya', 'pelanggan', 'created_at');
+    var $column_search = array('no_pelayanan', 'no_arsip', 'produk', 'tgl_berlaku', 'keberangkatan', 'durasi', 'tempat', 'biaya', 'pelanggan', 'created_at'); //field yang diizin untuk pencarian
+    var $order = array('id' => 'desc');
+
+    // UNTUK QUERY DATA TABLE
+    function _get_datatables_query()
+    {
+        $this->db->from($this->table);
+
+        $i = 0;
+
+        foreach ($this->column_search as $item) // looping awal
+        {
+            if ($_POST['search']['value']) // jika datatable mengirimkan pencarian dengan metode POST
+            {
+
+                if ($i === 0) // looping awal
+                {
+                    $this->db->group_start();
+                    $this->db->like('tbl_land_arrangement.' . $item, $_POST['search']['value']);
+                } else {
+                    $this->db->or_like('tbl_land_arrangement.' . $item, $_POST['search']['value']);
+                }
+
+                if (count($this->column_search) - 1 == $i) {
+                    $this->db->group_end();
+                }
+            }
+            $i++;
+        }
+
+        if (isset($_POST['order'])) {
+            $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        } else if (isset($this->order)) {
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
+    }
+
+    // UNTUK MENAMPILKAN HASIL QUERY KE DATA TABLES
+    function get_datatables()
+    {
+        $this->_get_datatables_query();
+        if ($_POST['length'] != -1)
+            $this->db->limit($_POST['length'], $_POST['start']);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    function count_filtered()
+    {
+        $this->_get_datatables_query();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    public function count_all()
+    {
+        $this->db->from($this->table);
+
+        return $this->db->count_all_results();
+    }
+
+    // Fungsi untuk format tanggal dalam bahasa Indonesia
+    private function format_tanggal_indo($tanggal)
+    {
+        $bulan = array(
+            1 => 'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+        );
+
+        // Pisahkan tanggal menjadi array
+        $tanggal_pisah = explode('-', $tanggal);
+        $tahun = $tanggal_pisah[0];
+        $bulan_indonesia = $bulan[(int)$tanggal_pisah[1]];
+        $hari = $tanggal_pisah[2];
+
+        // Return format "tanggal bulan tahun"
+        return $hari . ' ' . $bulan_indonesia . ' ' . $tahun;
+    }
+
+    // Fungsi yang memanggil format_tanggal_indo berkali-kali
+    public function getTanggal($tanggalValue)
+    {
+        // Hilangkan waktu jika ada, dengan memotong hanya bagian tanggal
+        $tanggal_only = explode(' ', $tanggalValue)[0]; // Pisahkan tanggal dari waktu
+
+        // Konversi ke format "tanggal bulan tahun" bahasa Indonesia
+        $formatted_tgl = $this->format_tanggal_indo($tanggal_only);
+
+        return $formatted_tgl;
+    }
+
+    // Fungsi lain yang juga bisa memanggil format_tanggal_indo
+    public function anotherMethod($tanggalValue)
+    {
+        $formatted_tgl = $this->format_tanggal_indo($tanggalValue);
+        return $formatted_tgl;
+    }
+
+
+    public function getPenawaran($id)
+    {
+        $data = $this->db->from('tbl_land_arrangement')->where('id', $id)->get()->row();
+        return $data;
+    }
+
+    public function max_kode($date)
+    {
+        $formatted_date = date('Y', strtotime($date));
+        $this->db->select('no_pelayanan');
+        $where = 'id=(SELECT max(id) FROM tbl_land_arrangement where SUBSTRING(no_pelayanan, 17, 4) = ' . $formatted_date . ')';
+        $this->db->where($where);
+        $query = $this->db->from('tbl_land_arrangement')->get();
+        return $query;
+    }
+
+    public function max_kode_arsip($date)
+    {
+        $formatted_date = date('y', strtotime($date));
+        $this->db->select('no_arsip');
+        $where = 'id=(SELECT max(id) FROM tbl_arsip_pu where SUBSTRING(no_arsip, 3, 2) = ' . $formatted_date . ')';
+        $this->db->where($where);
+        $query = $this->db->from('tbl_arsip_pu')->get();
+        return $query;
+    }
+
+    public function save($data)
+    {
+        $this->db->insert('tbl_land_arrangement', $data);
+        return $this->db->insert_id();
+    }
+}
---
+++ application/views/backend/penawaran_pu/penawaran_list_la_pu.php
@@ -0,0 +1,142 @@
+<style>
+    #appFilter {
+        border: 1px solid #ccc;
+        padding: 5px;
+        border-radius: 4px;
+    }
+</style>
+
+<div class="container-fluid">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><?= $titleview ?></h1>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3 d-flex justify-content-between align-items-center">
+                    <?php if ($add == 'Y') { ?>
+                        <a class="btn btn-primary btn-sm" href="<?= base_url('penawaran_la_pu/add_form') ?>">
+                            <i class="fa fa-plus"></i>&nbsp;Add Data
+                        </a>
+                    <?php } ?>
+                </div>
+
+                <div class="card-body">
+                    <table id="table" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Action</th>
+                                <th>Kode Pelayanan</th>
+                                <th>No Arsip</th>
+                                <th>Produk</th>
+                                <th>Tanggal Berlaku</th>
+                                <th>Keberangkatan</th>
+                                <th>Durasi</th>
+                                <th>Tempat</th>
+                                <th>Biaya</th>
+                                <th>Pelanggan</th>
+                                <th>Tanggal Dokumen</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>No</th>
+                                <th>Action</th>
+                                <th>Kode Pelayanan</th>
+                                <th>No Arsip</th>
+                                <th>Produk</th>
+                                <th>Tanggal Berlaku</th>
+                                <th>Keberangkatan</th>
+                                <th>Durasi</th>
+                                <th>Tempat</th>
+                                <th>Biaya</th>
+                                <th>Pelanggan</th>
+                                <th>Tanggal Dokumen</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php $this->load->view('template/footer'); ?>
+<?php $this->load->view('template/script'); ?>
+
+<script type="text/javascript">
+    var table;
+
+    // METHOD POST MENAMPILKAN DATA KE DATA TABLE
+    $(document).ready(function() {
+        table = $('#table').DataTable({
+            "responsive": false,
+            "scrollX": true,
+            "processing": true,
+            "serverSide": true,
+            "order": [],
+            "ajax": {
+                "url": "<?php echo site_url('penawaran_la_pu/get_list') ?>",
+                "type": "POST",
+                "data": function(d) {
+                    d.status = $('#appFilter').val(); // Tambahkan parameter status ke permintaan server
+                    d.tab = $('.nav-tabs .nav-link.active').data('tab'); // Tambahkan parameter tab ke permintaan server
+                }
+            },
+            // "language": {
+            //     "infoFiltered": ""
+            // },
+            "columnDefs": [{
+                    "targets": [2, 3, 5, 6],
+                    "className": 'dt-head-nowrap'
+                },
+                {
+                    "targets": [1, 3, 4],
+                    "className": 'dt-body-nowrap'
+                }, {
+                    "targets": [0, 1],
+                    "orderable": false,
+                },
+            ]
+        });
+    });
+
+    // MENGHAPUS DATA MENGGUNAKAN METHODE POST JQUERY
+    function delete_data(id) {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: "<?php echo site_url('penawaran_la_pu/delete/') ?>" + id,
+                    type: "POST",
+                    dataType: "JSON",
+                    success: function(data) {
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'success',
+                            title: 'Your data has been deleted',
+                            showConfirmButton: false,
+                            timer: 1500
+                        }).then((result) => {
+                            location.href = "<?= base_url('penawaran_la_pu') ?>";
+                        })
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        alert('Error deleting data');
+                    }
+                });
+            }
+        })
+    };
+</script>
