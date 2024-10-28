dd30ac8fd56350d4d8be1e9f93f073ec463e9f10
kymrs <muhamadrizkysaputra34@gmail.com>
2024-10-28 11:43:27 +0700

    p

--- application/views/backend/prepayment_sw/prepayment_form_sw.php
+++ application/views/backend/prepayment_sw/prepayment_form_sw.php
@@ -121,7 +121,7 @@
 
 <!-- MODAL -->
 <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">EVENT FORM</h5>
