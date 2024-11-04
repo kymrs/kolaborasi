868cc0e7b092185eae82b12cb2f8dc4849686800
kymrs <muhamadrizkysaputra34@gmail.com>
2024-11-04 15:59:12 +0700

    login page

--- application/views/backend/login.php
+++ application/views/backend/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="icon" href="<?= base_url() ?>assets/backend/icon/favicon.png" type="image/jpg">
+    <link rel="icon" href="favicon.ico" type="image/jpg">
     <link rel="stylesheet" href="<?= base_url() ?>assets/backend/plugins/sb2/vendor/fontawesome-free/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -24,10 +24,10 @@
     <div class="center">
         <div class="container">
             <div class="image">
-                <!-- <img src="<?= base_url() ?>assets/backend/img/kolaborasi1.png" alt="Logo Kolaborasi" class="logo"> -->
-                <h1>LOGIN</h1>
+                <img src="<?= base_url() ?>assets/backend/img/kolaborasi.png" alt="Logo Kolaborasi" class="logo">
+                <!-- <h1>LOGIN</h1> -->
             </div>
-            <div class="text-center">
+            <div class="text-center message">
                 <?= $this->session->flashdata('message'); ?>
             </div>
             <div class="form">
