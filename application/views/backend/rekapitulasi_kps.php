0eaa22a1f2ef78ec9d9eda9c696e03056e7b956d
kymrs <muhamadrizkysaputra34@gmail.com>
2024-11-14 08:44:49 +0700

    tes

--- application/views/backend/rekapitulasi_kps.php
+++ application/views/backend/rekapitulasi_kps.php
@@ -35,6 +35,12 @@
         margin-left: 10px;
     }
 
+    .main-filter {
+        cursor: pointer;
+        margin: 7px 0;
+        width: 150px;
+    }
+
     @media (max-width: 1000px) {
         .tgl-header {
             display: inline-block;
@@ -59,6 +65,16 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="card shadow mb-4">
+                <div class="card-header">
+                    <label for="appFilter" class="mr-2 mb-0">Main Filter:</label>
+                    <select id="appFilter" name="appFilter" class="form-control main-filter">
+                        <!-- <option value="" selected>Show all....</option> -->
+                        <option value="on-process" selected>All</option>
+                        <option value="approved">KPS</option>
+                        <option value="approved">pengenumroh</option>
+                        <option value="revised">sebelaswarna</option>
+                    </select>
+                </div>
                 <div class="card-header py-3 justify-content-start align-items-center tgl-header">
                     <div class="d-flex align-items-center mr-3 w-30 tgl-awal">
                         <label for="tgl_awal" class="mr-2 mb-0 tgl-awal-text">Tanggal Awal:</label>
