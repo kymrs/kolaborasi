5bcb23c4d7d445bfc4b9bb54d8ce4799c65c90e3
Aruudo09 <audricafabiano@gmail.com>
2025-01-06 17:03:32 +0700

    pushtcpdf

---
+++ application/libraries/T_cpdf2.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+// Memuat file TCPDF dari folder third_party
+require_once(APPPATH . 'third_party/TCPDF-main/tcpdf.php');
+
+class T_cpdf2 extends TCPDf
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
