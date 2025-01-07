a13f2ce47b1d1c5e1ae74bebc0042d55e2460691
Aruudo09 <audricafabiano@gmail.com>
2025-01-07 08:17:56 +0700

    pushae

--- application/controllers/invoice_pu.php
+++ application/controllers/invoice_pu.php
@@ -1,12 +1,30 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+// Memuat file TCPDF dari folder third_party
+require_once(APPPATH . 'third_party/TCPDF-main/tcpdf.php');
+
+class T_cpdf2 extends TCPDf
+{
+    // Page header
+    function Header()
+    {
+        // Logo
+        $this->SetFont('helvetica', 'B', 12);
+        $this->Image('assets/backend/img/pengenumroh.png', 5, 3, 37, 20);
+        $this->SetX(120);
+        $this->SetFont('Poppins-Regular', '', 9);
+        $this->Cell(40, 5, 'Keberangkatan', 0, 0);
+        $this->Ln(5);
+    }
+}
+
 class Invoice_pu extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('backend/M_invoice_pu');
+        $this->load->model('backend/M_prepayment_pu');
         $this->load->model('backend/M_notifikasi');
         $this->M_login->getsecurity();
         date_default_timezone_set('Asia/Jakarta');
@@ -74,7 +92,7 @@ class Invoice_pu extends CI_Controller
             ->where('id_user', $this->session->userdata('id_user'))
             ->get()
             ->row('name');
-        $list = $this->M_invoice_pu->get_datatables();
+        $list = $this->M_prepayment_pu->get_datatables();
         $data = array();
         $no = $_POST['start'];
 
@@ -143,8 +161,8 @@ class Invoice_pu extends CI_Controller
 
         $output = array(
             "draw" => $_POST['draw'],
-            "recordsTotal" => $this->M_invoice_pu->count_all(),
-            "recordsFiltered" => $this->M_invoice_pu->count_filtered(),
+            "recordsTotal" => $this->M_prepayment_pu->count_all(),
+            "recordsFiltered" => $this->M_prepayment_pu->count_filtered(),
             "data" => $data,
         );
         //output dalam format JSON
@@ -167,27 +185,26 @@ class Invoice_pu extends CI_Controller
         $data['id'] = 0;
         $data['title'] = 'backend/pu_invoice/pu_invoice_form';
         $data['title_view'] = 'Invoice Form';
-        $data['rek_options'] = $this->M_invoice_pu->options()->result_array();
+        $data['rek_options'] = $this->M_prepayment_pu->options()->result_array();
         $data['notif'] = $this->M_notifikasi->pending_notification();
         $this->load->view('backend/home', $data);
     }
 
-    // MEREGENERATE KODE INVOICE
+    // MEREGENERATE KODE PREPAYMENT
     public function generate_kode()
     {
         $date = $this->input->post('date');
-
-        $kode = $this->M_invoice_pu->max_kode($date)->row();
-
-        if (empty($kode->kode_invoice)) {
+        $kode = $this->M_prepayment_pu->max_kode($date)->row();
+        if (empty($kode->kode_prepayment)) {
             $no_urut = 1;
         } else {
-            $bln = substr($kode->kode_invoice, 5, 2);
-            $no_urut = substr($kode->kode_invoice, 10) + 1;
+            $bln = substr($kode->kode_prepayment, 3, 2);
+            $no_urut = substr($kode->kode_prepayment, 5) + 1;
         }
         $urutan = str_pad($no_urut, 4, "0", STR_PAD_LEFT);
         $month = substr($date, 3, 2);
-        $data = 'INVPU' . $month . $urutan;
+        $year = substr($date, 8, 2);
+        $data = 'p' . $year . $month . $urutan;
         echo json_encode($data);
     }
 
@@ -198,15 +215,15 @@ class Invoice_pu extends CI_Controller
         $data['id'] = $id;
         $data['aksi'] = 'update';
         $data['title_view'] = "Edit Data Prepayment";
-        $data['rek_options'] = $this->M_invoice_pu->options()->result_array();
+        $data['rek_options'] = $this->M_prepayment_pu->options()->result_array();
         $data['title'] = 'backend/prepayment_pu/prepayment_form_pu';
         $this->load->view('backend/home', $data);
     }
 
     function edit_data($id)
     {
-        $data['master'] = $this->M_invoice_pu->get_by_id($id);
-        $data['transaksi'] = $this->M_invoice_pu->get_by_id_detail($id);
+        $data['master'] = $this->M_prepayment_pu->get_by_id($id);
+        $data['transaksi'] = $this->M_prepayment_pu->get_by_id_detail($id);
         $data['nama'] = $this->db->select('name')
             ->from('tbl_data_user')
             ->where('id_user', $data['master']->id_user)
@@ -216,7 +233,7 @@ class Invoice_pu extends CI_Controller
 
     function read_detail($id)
     {
-        $data = $this->M_invoice_pu->get_by_id_detail($id);
+        $data = $this->M_prepayment_pu->get_by_id_detail($id);
         echo json_encode($data);
     }
 
@@ -225,7 +242,7 @@ class Invoice_pu extends CI_Controller
     {
         // INSERT KODE PREPAYMENT SAAT SUBMIT
         $date = $this->input->post('tgl_prepayment');
-        $kode = $this->M_invoice_pu->max_kode($date)->row();
+        $kode = $this->M_prepayment_pu->max_kode($date)->row();
         if (empty($kode->kode_prepayment)) {
             $no_urut = 1;
         } else {
@@ -237,6 +254,66 @@ class Invoice_pu extends CI_Controller
         $year = substr($date, 8, 2);
         $kode_prepayment = 'P' . $year . $month . $urutan;
 
+        // MENCARI SIAPA YANG AKAN MELAKUKAN APPROVAL PERMINTAAN
+        $approval = $this->M_prepayment_pu->approval($this->session->userdata('id_user'));
+        $id = $this->session->userdata('id_user');
+
+        // CHECK APAKAH MENGINPUT YANG SUDAH ADA ATAU YANG BARU (REKENING)
+        if (!empty($_POST['nama_rek'])) {
+            $no_rek = $this->input->post('nama_rek') . "-" . $this->input->post('nama_bank') . "-" . $this->input->post('nomor_rekening');
+        } else {
+            $no_rek = $this->input->post('rekening');
+        }
+
+        $data = array(
+            'kode_prepayment' => $kode_prepayment,
+            'id_user' => $id,
+            'prepayment' => $this->input->post('prepayment'),
+            'tujuan' => $this->input->post('tujuan'),
+            'tgl_prepayment' => date('Y-m-d', strtotime($this->input->post('tgl_prepayment'))),
+            'total_nominal' => $this->input->post('total_nominal'),
+            'no_rek' => $no_rek,
+            'divisi' => $this->db->select('divisi')
+                ->from('tbl_data_user')
+                ->where('id_user', $id)
+                ->get()
+                ->row('divisi'),
+            'jabatan' => $this->db->select('jabatan')
+                ->from('tbl_data_user')
+                ->where('id_user', $id)
+                ->get()
+                ->row('jabatan'),
+            'app_name' => $this->db->select('name')
+                ->from('tbl_data_user')
+                ->where('id_user', $approval->app_id)
+                ->get()
+                ->row('name'),
+            'app2_name' => $this->db->select('name')
+                ->from('tbl_data_user')
+                ->where('id_user', $approval->app2_id)
+                ->get()
+                ->row('name'),
+            'created_at' => date('Y-m-d H:i:s')
+        );
+
+        $inserted = $this->M_prepayment_pu->save($data);
+
+        if ($inserted) {
+            // INISIASI VARIABEL INPUT DETAIL PREPAYMENT
+            $rincian = $this->input->post('rincian[]');
+            $nominal = $this->input->post('hidden_nominal[]');
+            $keterangan = $this->input->post('keterangan[]');
+            //PERULANGAN UNTUK INSER QUERY DETAIL PREPAYMENT
+            for ($i = 1; $i <= count($_POST['rincian']); $i++) {
+                $data2[] = array(
+                    'prepayment_id' => $inserted,
+                    'rincian' => $rincian[$i],
+                    'nominal' => $nominal[$i],
+                    'keterangan' => $keterangan[$i]
+                );
+            }
+            $this->M_prepayment_pu->save_detail($data2);
+        }
         echo json_encode(array("status" => TRUE));
     }
 
@@ -305,8 +382,8 @@ class Invoice_pu extends CI_Controller
     // MENGHAPUS DATA
     function delete($id)
     {
-        $this->M_invoice_pu->delete($id);
-        $this->M_invoice_pu->delete_detail($id);
+        $this->M_prepayment_pu->delete($id);
+        $this->M_prepayment_pu->delete_detail($id);
         echo json_encode(array("status" => TRUE));
     }
 
@@ -359,206 +436,29 @@ class Invoice_pu extends CI_Controller
         echo json_encode(array("status" => TRUE));
     }
 
-    public function generate_pdf($id)
+    // PRINTOUT TCPDF
+    public function generate_pdf2()
     {
-        // Load FPDF library
-        $this->load->library('Fpdf_generate');
 
-        // Load data from database based on $id
-        $data['master'] = $this->M_invoice_pu->get_by_id($id);
-        $data['transaksi'] = $this->M_invoice_pu->get_by_id_detail($id);
-        $data['user'] = $this->db->select('name')
-            ->from('tbl_data_user')
-            ->where('id_user', $data['master']->id_user)
-            ->get()
-            ->row('name');
-        $data['app_status'] = strtoupper($data['master']->app_status);
-        $data['app2_status'] = strtoupper($data['master']->app2_status);
+        // INISIAI VARIABLE
+        // $penawaran = $this->M_penawaran_la_pu->getPenawaran($id);
+        // $rundowns = $this->M_penawaran_la_pu->get_rundown($penawaran->no_pelayanan);
+        // $hotels = $this->M_penawaran_la_pu->get_hotels($id);
 
-        // Format tgl_prepayment to Indonesian date
-        $tanggal = $data['master']->tgl_prepayment;
-        $formatted_date = date('d F Y', strtotime($tanggal));
-        $months = [
-            'January' => 'Januari',
-            'February' => 'Februari',
-            'March' => 'Maret',
-            'April' => 'April',
-            'May' => 'Mei',
-            'June' => 'Juni',
-            'July' => 'Juli',
-            'August' => 'Agustus',
-            'September' => 'September',
-            'October' => 'Oktober',
-            'November' => 'November',
-            'December' => 'Desember'
-        ];
-        $month = date('F', strtotime($tanggal));
-        $translated_month = $months[$month];
-        $formatted_date = str_replace($month, $translated_month, $formatted_date);
+        // Initialize the TCPDF object
+        $t_cpdf2 = new t_cpdf2('P', 'mm', 'A5', true, 'UTF-8', false);
 
-        // Start FPDF
-        $pdf = new Fpdf_generate('P', 'mm', 'A4');
-        $pdf->SetTitle('Form Pengajuan Prepayment');
-        $pdf->AddPage('P', 'Letter');
+        // Set document properties
+        $t_cpdf2->SetCreator(PDF_CREATOR);
+        $t_cpdf2->SetAuthor('Author Name');
+        $t_cpdf2->SetTitle('Penawaran PDF');
 
-        // Logo
-        $pdf->Image(base_url('') . '/assets/backend/img/pengenumroh.png', 11.5, 3, 35, 22);
+        $t_cpdf2->SetMargins(15, 38, 15); // Margin kiri, atas (untuk header), kanan
+        // $t_cpdf2->SetHeaderMargin(40);    // Jarak antara header dan konten
+        $t_cpdf2->SetAutoPageBreak(true, 40); // Penanganan otomatis margin bawah
 
-        $pdf->AddFont('Poppins-Regular', '', 'Poppins-Regular.php');
-        $pdf->AddFont('Poppins-Bold', '', 'Poppins-Bold.php');
-
-        $pdf->Ln(17);
-        $pdf->SetFont('Poppins-Regular', '', 12);
-        $pdf->Cell(30, 10, 'Divisi', 0, 0);
-        $pdf->Cell(5, 10, ':', 0, 0);
-        $pdf->Cell(50, 10, $data['master']->divisi, 0, 1);
-
-        // $pdf->SetX(46); // Tetap di posisi yang sama untuk elemen lainnya
-        $pdf->Cell(30, 10, 'Prepayment', 0, 0);
-        $pdf->Cell(5, 10, ':', 0, 0);
-        $pdf->Cell(50, 10, $data['master']->prepayment, 0, 1);
-        $pdf->Ln(8);
-
-        // Form Title
-        $pdf->SetFont('Poppins-Bold', '', 14);
-        $pdf->Cell(0, 10, 'FORM PENGAJUAN PREPAYMENT', 0, 1, 'C');
-        $pdf->Ln(5);
-
-        // Form Fields
-        $pdf->SetFont('Poppins-Regular', '', 12);
-        $pdf->Cell(30, 10, 'Tanggal', 0, 0);
-        $pdf->Cell(5, 10, ':', 0, 0);
-        $pdf->Cell(50, 10, $formatted_date, 0, 1);
-
-        $pdf->Cell(30, 10, 'Nama', 0, 0);
-        $pdf->Cell(5, 10, ':', 0, 0);
-        $pdf->Cell(50, 10, $data['user'], 0, 1);
-
-        $pdf->Cell(30, 10, 'Jabatan', 0, 0);
-        $pdf->Cell(5, 10, ':', 0, 0);
-        $pdf->Cell(50, 10, $data['master']->jabatan, 0, 1);
-
-        $pdf->Cell(60, 10, 'Dengan ini bermaksud mengajukan prepayment untuk :', 0, 1);
-
-        $pdf->Cell(30, 10, 'Tujuan', 0, 0);
-        $pdf->Cell(5, 10, ':', 0, 0);
-        $pdf->Cell(50, 10, $data['master']->tujuan, 0, 1);
-        $pdf->Ln(5);
-
-        // Table Header
-        $pdf->SetFont('Poppins-Bold', '', 12);
-        $pdf->Cell(55, 10, 'Rincian', 1, 0, 'C');
-        $pdf->Cell(55, 10, 'Nominal', 1, 0, 'C');
-        $pdf->Cell(79, 10, 'Keterangan', 1, 1, 'C');
-
-        // Table Content
-        $pdf->SetFont('Poppins-Regular', '', 12);
-        $pdf->SetFillColor(255, 255, 255); // Row color
-
-        foreach ($data['transaksi'] as $row) {
-            // Hitung tinggi maksimum dari baris ini berdasarkan jumlah baris dari masing-masing kolom
-            $rincianLines = $pdf->GetStringWidth($row['rincian']) / 55; // 55 adalah lebar kolom rincian
-            $rincianHeight = ceil($rincianLines) * 7; // Estimasi tinggi kolom rincian (ubah dari 10 ke 7)
-
-            $keteranganLines = $pdf->GetStringWidth($row['keterangan']) / 79; // 79 adalah lebar kolom keterangan
-            $keteranganHeight = ceil($keteranganLines) * 7; // Estimasi tinggi kolom keterangan
-
-            // Ambil tinggi maksimum dari kedua kolom
-            $height = max($rincianHeight, $keteranganHeight, 7);
-            $height2 = max($rincianHeight, $keteranganHeight, 7);
-            if ($rincianHeight > 7) {
-                $height = 7;
-            }
-            if ($keteranganHeight > 7) {
-                $height2 = 7;
-            }
-            $maxHeight = max($rincianHeight, $keteranganHeight, 7); // Pastikan tinggi minimum 7
-
-            // Kolom rincian (gunakan MultiCell untuk wrapping)
-            $x = $pdf->GetX();
-            $y = $pdf->GetY();
-            $pdf->MultiCell(55, $height, $row['rincian'], 1, 'C', true); // Tinggi kolom rincian 7
-            $pdf->SetXY($x + 55, $y); // Pindahkan ke posisi kolom selanjutnya
-
-            // Kolom nominal (gunakan MultiCell untuk penyesuaian tinggi)
-            $x = $pdf->GetX();
-            $y = $pdf->GetY();
-            $pdf->MultiCell(55, $maxHeight, number_format($row['nominal'], 0, ',', '.'), 1, 'C', true); // Tinggi sesuai maxHeight
-            $pdf->SetXY($x + 55, $y); // Pindahkan ke posisi kolom selanjutnya
-
-            // Kolom keterangan (gunakan MultiCell untuk wrapping)
-            $x = $pdf->GetX();
-            $y = $pdf->GetY();
-            $pdf->MultiCell(79, $height2, $row['keterangan'], 1, 'C', true); // Tinggi sesuai maxHeight
-            $pdf->SetXY($x + 79, $y); // Pindahkan ke posisi kolom selanjutnya
-
-            // Pindahkan ke baris baru
-            $pdf->Ln($maxHeight);
-        }
-
-        // Table Footer
-        $pdf->SetFont('Poppins-Bold', '', 12);
-        $pdf->Cell(55, 10, 'Total :', 1, 0, 'C');
-        $pdf->SetFont('Arial', '', 12);
-        $pdf->Cell(134, 10, number_format($data['master']->total_nominal, 0, ',', '.'), 1, 1, 'C');
-        $pdf->Ln(10);
-
-        //APPROVAL
-        $pdf->SetFont('Poppins-Bold', '', 12);
-
-        // Membuat header tabel
-        $pdf->Cell(63, 8.5, 'Yang Melakukan', 1, 0, 'C');
-        $pdf->Cell(63, 8.5, 'Mengetahui', 1, 0, 'C');
-        $pdf->Cell(63, 8.5, 'Menyetujui', 1, 1, 'C');
-
-        // Set font normal untuk konten tabel
-        $pdf->SetFont('Poppins-Regular', '', 10);
-
-        // Baris pemisah
-        $pdf->Cell(63, 5, '', 'LR', 0, 'C');
-        $pdf->Cell(63, 5, '', 0, 0, 'C');
-        $pdf->Cell(63, 5, '', 'LR', 1, 'C');
-
-        // Baris pertama (Status)
-        $pdf->Cell(63, 5, 'CREATED', 'LR', 0, 'C');
-        $pdf->Cell(63, 5, strtoupper($data['master']->app_status), 0, 0, 'C');
-        $pdf->Cell(63, 5, strtoupper($data['master']->app2_status), 'LR', 1, 'C');
-
-        // Baris kedua (Tanggal)
-        $pdf->Cell(63, 5, $data['master']->created_at, 'LR', 0, 'C');
-        $pdf->Cell(63, 5, $data['master']->app_date, 0, 0, 'C');
-        $pdf->Cell(63, 5, $data['master']->app2_date, 'LR', 1, 'C');
-
-        // Baris pemisah
-        $pdf->Cell(63, 5, '', 'LR', 0, 'C');
-        $pdf->Cell(63, 5, '', 0, 0, 'C');
-        $pdf->Cell(63, 5, '', 'LR', 1, 'C');
-
-        // Jarak kosong untuk pemisah
-        $pdf->Ln(0);
-
-        // Baris ketiga (Nama pengguna)
-        $pdf->Cell(63, 8.5, $data['user'], 1, 0, 'C');
-        $pdf->Cell(63, 8.5, $data['master']->app_name, 1, 0, 'C');
-        $pdf->Cell(63, 8.5, $data['master']->app2_name, 1, 1, 'C');
-
-
-        // Add keterangan
-        $pdf->Ln(5);
-        $pdf->SetFont('Poppins-Regular', '', 12);
-        if (($data['master']->app_keterangan != null && $data['master']->app_keterangan != '') || ($data['master']->app2_keterangan != null && $data['master']->app2_keterangan != '')) {
-            $pdf->Cell(40, 10, 'Keterangan:', 0, 0);
-        }
-        $pdf->Ln(8);
-        if ($data['master']->app_keterangan != null && $data['master']->app_keterangan != '') {
-            $pdf->Cell(60, 10, '*' . '(' . $data['master']->app_name . ') ' . $data['master']->app_keterangan, 0, 1);
-        }
-        if ($data['master']->app2_keterangan != null && $data['master']->app2_keterangan != '') {
-            $pdf->Cell(60, 10, '*' . '(' . $data['master']->app2_name . ') ' . $data['master']->app2_keterangan, 0, 1);
-        }
-
-        // Output the PDF
-        $pdf->Output('I', 'Prepayment_PU.pdf');
+        // Add a new page
+        $t_cpdf2->AddPage();
     }
 
     // QUERY UNTUK INPUT TANDA TANGAN
