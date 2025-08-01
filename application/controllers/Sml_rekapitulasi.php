bfc5bb88f1c7ad1160c52fc6b135398301042e9b
Aruudo09 <audricafabiano@gmail.com>
2025-08-01 09:22:00 +0700

    pushae

--- application/controllers/Sml_rekapitulasi.php
+++ application/controllers/Sml_rekapitulasi.php
@@ -98,99 +98,9 @@ class Sml_rekapitulasi extends CI_Controller
         echo json_encode($output);
     }
 
-<<<<<<< HEAD
-    public function get_list_pelaporan()
-    {
-        $list = $this->M_sml_rekapitulasi->get_datatables_pelaporan();
-        $data = array();
-        $no = $_POST['start'];
-
-        foreach ($list as $field) {
-            $no++;
-            $row = array();
-            $row[] = $no;
-            $row[] = $field->kode_prepayment ? $field->kode_prepayment : '-';
-            $row[] = !empty($field->kode_reimbust) ? ucfirst($field->kode_reimbust) : '-';
-            $row[] = $this->tgl_indo($field->tgl_pengajuan);
-            $row[] = $field->tujuan;
-            $row[] = 'Rp. ' . number_format($field->total_nominal, 0, ',', '.');
-            $data[] = $row;
-        }
-
-        $output = array(
-            "draw" => $_POST['draw'],
-            "recordsTotal" => $this->M_sml_rekapitulasi->count_all_pelaporan(),
-            "recordsFiltered" => $this->M_sml_rekapitulasi->count_filtered_pelaporan(),
-            "data" => $data,
-        );
-        echo json_encode($output);
-    }
-
-    public function get_list_reimbust()
-    {
-        $list = $this->M_sml_rekapitulasi->get_datatables_reimbust();
-        $data = array();
-        $no = $_POST['start'];
-
-        foreach ($list as $field) {
-            $no++;
-            $row = array();
-            $row[] = $no;
-            $row[] = '-';
-            $row[] = !empty($field->kode_reimbust) ? strtoupper($field->kode_reimbust) : '-';
-            $row[] = $this->tgl_indo($field->tgl_pengajuan);
-            $row[] = $field->tujuan;
-            $row[] = 'Rp. ' . number_format($field->total_jumlah_detail, 0, ',', '.');
-            $data[] = $row;
-        }
-
-        $output = array(
-            "draw" => $_POST['draw'],
-            "recordsTotal" => $this->M_sml_rekapitulasi->count_all_reimbust(),
-            "recordsFiltered" => $this->M_sml_rekapitulasi->count_filtered_reimbust(),
-            "data" => $data,
-        );
-        echo json_encode($output);
-    }
-
-    public function get_list_invoice()
-    {
-        $list = $this->M_sml_rekapitulasi->get_datatables_invoice();
-        $data = array();
-        $no = $_POST['start'];
-
-        foreach ($list as $field) {
-            $no++;
-            $row = array();
-            $row[] = $no;
-            $row[] = !empty($field->kode_invoice) ? $field->kode_invoice : '-';
-            $row[] = $this->tgl_indo($field->tgl_invoice);
-            $row[] = $field->ctc_to;
-            $row[] = 'Rp. ' . number_format($field->total, 0, ',', '.');
-            $row[] = $field->payment_status == 1 ? 'Lunas' : 'Belum Lunas';
-            $data[] = $row;
-        }
-
-        $output = array(
-            "draw" => $_POST['draw'],
-            "recordsTotal" => $this->M_sml_rekapitulasi->count_all_invoice(),
-            "recordsFiltered" => $this->M_sml_rekapitulasi->count_filtered_invoice(),
-            "data" => $data,
-        );
-        echo json_encode($output);
-    }
-
-    function get_total()
-    {
-        $output = array(
-            'pengeluaran' => $this->M_sml_rekapitulasi->get_total_pengeluaran(),
-            'pemasukan' => $this->M_sml_rekapitulasi->get_total_pemasukan()
-        );
-=======
     function get_total()
     {
         $output = $this->M_sml_rekapitulasi->get_total_pengeluaran();
->>>>>>> 01baacc0bc7ac03d364f8779f5d23138bd323641
 
         //output dalam format JSON
         echo json_encode($output);
