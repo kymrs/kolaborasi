1a7ff818581b4637df39e1ffe6b181d7d635fc5c
kymrs <muhamadrizkysaputra34@gmail.com>
2026-01-15 11:39:29 +0700

    kertas kerja view

--- application/controllers/Sml_kertas_kerja.php
+++ application/controllers/Sml_kertas_kerja.php
@@ -16,17 +16,18 @@ class Sml_kertas_kerja extends CI_Controller
         $akses = $this->M_app->hak_akses($this->session->userdata('id_level'), $this->router->fetch_class());
         ($akses->view_level == 'N' ? redirect('auth') : '');
         $data['add'] = $akses->add_level;
-        $data['title'] = "backend/sml_kertas_kerja/sml_kertaskerja_list";
-        $data['titleview'] = "Data Minutes Of Meeting";
+        $data['title'] = "backend/sml_kertas_kerja/sml_kertas_kerja_list";
+        $data['titleview'] = "Kertas Kerja";
         $this->load->view('backend/home', $data);
     }
 
     function get_list()
     {
-        $list = $this->M_sml_kertas_kerja-->get_datatables();
+        $list = $this->M_sml_kertas_kerja->get_datatables();
         $data = array();
         $no = $_POST['start'];
         $akses = $this->M_app->hak_akses($this->session->userdata('id_level'), $this->router->fetch_class());
+
         foreach ($list as $field) {
             $no++;
             $row = array();
@@ -48,8 +49,8 @@ class Sml_kertas_kerja extends CI_Controller
 
         $output = array(
             "draw" => $_POST['draw'],
-            "recordsTotal" => $this->M_sml_kertas_kerja-->count_all(),
-            "recordsFiltered" => $this->M_sml_kertas_kerja-->count_filtered(),
+            "recordsTotal" => $this->M_sml_kertas_kerja->count_all(),
+            "recordsFiltered" => $this->M_sml_kertas_kerja->count_filtered(),
             "data" => $data,
         );
         //output dalam format JSON
@@ -58,11 +59,11 @@ class Sml_kertas_kerja extends CI_Controller
 
     function read_form($id)
     {
-        $data['data'] = $this->M_sml_kertas_kerja-->get_by_id($id);
+        $data['data'] = $this->M_sml_kertas_kerja->get_by_id($id);
         $data['aksi'] = 'read';
         $data['id'] = $id;
         $data['title_view'] = "Data Minutes Of Meeting";
-        $data['title'] = 'backend/sml_kertas_kerja/sml_kertaskerja_form';
+        $data['title'] = 'backend/sml_kertas_kerja/sml_kertas_kerja_form';
         $this->load->view('backend/home', $data);
     }
 
@@ -70,29 +71,28 @@ class Sml_kertas_kerja extends CI_Controller
     {
         $data['id'] = 0;
         $data['title_view'] = "Data Minutes Of Meeting";
-        $data['title'] = 'backend/sml_kertas_kerja/sml_kertaskerja_form';
+        $data['title'] = 'backend/sml_kertas_kerja/sml_kertas_kerja_form';
         $this->load->view('backend/home', $data);
     }
 
     function edit_form($id)
     {
-        $data['data'] = $this->M_sml_kertas_kerja-->get_by_id($id);
+        $data['data'] = $this->M_sml_kertas_kerja->get_by_id($id);
         $data['id'] = $id;
         $data['title_view'] = "Edit Data Minutes Of Meeting";
-        $data['title'] = 'backend/sml_kertas_kerja/sml_kertaskerja_form';
+        $data['title'] = 'backend/sml_kertas_kerja/sml_kertas_kerja_form';
         $this->load->view('backend/home', $data);
     }
 
     function edit_data($id)
     {
-        $data = $this->M_sml_kertas_kerja-->get_by_id($id);
+        $data = $this->M_sml_kertas_kerja->get_by_id($id);
         echo json_encode($data);
     }
 
     public function add()
     {
         $data = array(
-            'no_dok' => $no_dok,
             'agenda' => $this->input->post('agenda'),
             'date' => date('Y-m-d', strtotime($this->input->post('date'))),
             'start_time' => $this->input->post('start_time'),
@@ -100,11 +100,10 @@ class Sml_kertas_kerja extends CI_Controller
             'lokasi' => $this->input->post('lokasi'),
             'peserta' => $this->input->post('peserta'),
             'konten' => $this->input->post('konten'),
-            'foto' => $foto,
             'user' => $this->session->userdata('fullname'),
             'created_at' => date('Y-m-d H:i:s'),
         );
-        $this->M_sml_kertas_kerja-->save($data);
+        $this->M_sml_kertas_kerja->save($data);
         echo json_encode(array("status" => TRUE));
     }
 
@@ -118,7 +117,6 @@ class Sml_kertas_kerja extends CI_Controller
             'lokasi' => $this->input->post('lokasi'),
             'peserta' => $this->input->post('peserta'),
             'konten' => $this->input->post('konten'),
-            'foto' => $foto,
             'user' => $this->session->userdata('fullname'),
             'edit_at' =>  date('Y-m-d H:i:s'),
         );
@@ -129,7 +127,7 @@ class Sml_kertas_kerja extends CI_Controller
 
     function delete($id)
     {
-        $this->M_sml_kertas_kerja-->delete($id);
+        $this->M_sml_kertas_kerja->delete($id);
         echo json_encode(array("status" => TRUE));
     }
 
