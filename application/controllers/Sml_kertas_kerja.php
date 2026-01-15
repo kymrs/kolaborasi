75f8c71eafea181fddfea858665a0a8f6eec41a2
rakha <babanrakha@gmail.com.com>
2026-01-15 09:54:17 +0700

    kertaskerja

---
+++ application/controllers/Sml_kertas_kerja.php
@@ -0,0 +1,136 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sml_kertas_kerja extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('backend/M_sml_kertas_kerja');
+        $this->M_login->getsecurity();
+    }
+
+    public function index()
+    {
+        $akses = $this->M_app->hak_akses($this->session->userdata('id_level'), $this->router->fetch_class());
+        ($akses->view_level == 'N' ? redirect('auth') : '');
+        $data['add'] = $akses->add_level;
+        $data['title'] = "backend/sml_kertas_kerja/sml_kertaskerja_list";
+        $data['titleview'] = "Data Minutes Of Meeting";
+        $this->load->view('backend/home', $data);
+    }
+
+    function get_list()
+    {
+        $list = $this->M_sml_kertas_kerja-->get_datatables();
+        $data = array();
+        $no = $_POST['start'];
+        $akses = $this->M_app->hak_akses($this->session->userdata('id_level'), $this->router->fetch_class());
+        foreach ($list as $field) {
+            $no++;
+            $row = array();
+            $row[] = $no;
+            $row[] = ($akses->view_level == 'Y' ? '<a href="sml_kertas_kerja/read_form/' . $field->id . '" class="btn btn-info btn-circle btn-sm" title="Read"><i class="fa fa-eye"></i></a>&nbsp;' : '') .
+                ($akses->edit_level == 'Y' ? '<a href="sml_kertas_kerja/edit_form/' . $field->id . '" class="btn btn-warning btn-circle btn-sm" title="Edit"><i class="fa fa-edit"></i></a>&nbsp;' : '') .
+                ($akses->delete_level == 'Y' ? '<a onclick="delete_data(' . "'" . $field->id . "'" . ')" class="btn btn-danger btn-circle btn-sm" title="Delete"><i class="fa fa-trash"></i></a>&nbsp;' : '') .
+                ($akses->print_level == 'Y' ? '<a href="sml_kertas_kerja/pdf/' . $field->id . '" target="_blank" class="btn btn-success btn-circle btn-sm" title="Read"><i class="far fa-file-pdf"></i></a>' : '');
+            $row[] = $field->no_dok;
+            $row[] = $field->agenda;
+            $row[] = $field->date;
+            $row[] = $field->start_time;
+            $row[] = $field->end_time;
+            $row[] = $field->lokasi;
+            $row[] = $field->peserta;
+
+            $data[] = $row;
+        }
+
+        $output = array(
+            "draw" => $_POST['draw'],
+            "recordsTotal" => $this->M_sml_kertas_kerja-->count_all(),
+            "recordsFiltered" => $this->M_sml_kertas_kerja-->count_filtered(),
+            "data" => $data,
+        );
+        //output dalam format JSON
+        echo json_encode($output);
+    }
+
+    function read_form($id)
+    {
+        $data['data'] = $this->M_sml_kertas_kerja-->get_by_id($id);
+        $data['aksi'] = 'read';
+        $data['id'] = $id;
+        $data['title_view'] = "Data Minutes Of Meeting";
+        $data['title'] = 'backend/sml_kertas_kerja/sml_kertaskerja_form';
+        $this->load->view('backend/home', $data);
+    }
+
+    function add_form()
+    {
+        $data['id'] = 0;
+        $data['title_view'] = "Data Minutes Of Meeting";
+        $data['title'] = 'backend/sml_kertas_kerja/sml_kertaskerja_form';
+        $this->load->view('backend/home', $data);
+    }
+
+    function edit_form($id)
+    {
+        $data['data'] = $this->M_sml_kertas_kerja-->get_by_id($id);
+        $data['id'] = $id;
+        $data['title_view'] = "Edit Data Minutes Of Meeting";
+        $data['title'] = 'backend/sml_kertas_kerja/sml_kertaskerja_form';
+        $this->load->view('backend/home', $data);
+    }
+
+    function edit_data($id)
+    {
+        $data = $this->M_sml_kertas_kerja-->get_by_id($id);
+        echo json_encode($data);
+    }
+
+    public function add()
+    {
+        $data = array(
+            'no_dok' => $no_dok,
+            'agenda' => $this->input->post('agenda'),
+            'date' => date('Y-m-d', strtotime($this->input->post('date'))),
+            'start_time' => $this->input->post('start_time'),
+            'end_time' => $this->input->post('end_time'),
+            'lokasi' => $this->input->post('lokasi'),
+            'peserta' => $this->input->post('peserta'),
+            'konten' => $this->input->post('konten'),
+            'foto' => $foto,
+            'user' => $this->session->userdata('fullname'),
+            'created_at' => date('Y-m-d H:i:s'),
+        );
+        $this->M_sml_kertas_kerja-->save($data);
+        echo json_encode(array("status" => TRUE));
+    }
+
+    public function update()
+    {
+        $data = array(
+            'agenda' => $this->input->post('agenda'),
+            'date' => date('Y-m-d', strtotime($this->input->post('date'))),
+            'start_time' => $this->input->post('start_time'),
+            'end_time' => $this->input->post('end_time'),
+            'lokasi' => $this->input->post('lokasi'),
+            'peserta' => $this->input->post('peserta'),
+            'konten' => $this->input->post('konten'),
+            'foto' => $foto,
+            'user' => $this->session->userdata('fullname'),
+            'edit_at' =>  date('Y-m-d H:i:s'),
+        );
+        $this->db->where('id', $this->input->post('id'));
+        $this->db->update('sml_kertas_kerja', $data);
+        echo json_encode(array("status" => TRUE));
+    }
+
+    function delete($id)
+    {
+        $this->M_sml_kertas_kerja-->delete($id);
+        echo json_encode(array("status" => TRUE));
+    }
+
+}
--- application/controllers/WeekCalculator.php
+++ application/controllers/WeekCalculator.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 class WeekCalculator extends CI_Controller
 {
@@ -16,54 +15,6 @@ class WeekCalculator extends CI_Controller
     }
 
     public function getWeek($year = null, $month = null, $day = null)
-=======
-// class WeekCalculator extends CI_Controller
-// {
-//     public function index($year = null, $month = null)
-//     {
-//         // Set default bulan dan tahun jika tidak diberikan
-//         $year = $year ?? date('Y');
-//         $month = $month ?? date('m');
-
-//         // Hitung jumlah hari dalam bulan tersebut
-//         $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-
-//         // Array untuk menyimpan hasil minggu
-//         $weeks = [];
-
-//         $weekNumber = 0;
-
-//         // Looping untuk setiap hari dalam bulan tersebut
-//         for ($day = 1; $day <= $daysInMonth; $day++) {
-//             $date = sprintf('%04d-%02d-%02d', $year, $month, $day);
-//             $dayOfWeek = date('w', strtotime($date)); // 0 = Minggu, 1 = Senin, ..., 6 = Sabtu
-
-//             // Tentukan minggu
-//             if ($day == 1) {
-//                 $weekNumber = 1; // Tanggal 1 selalu minggu pertama
-//             } elseif ($dayOfWeek == 0) {
-//                 $weekNumber++; // Hari Minggu menandai awal minggu baru
-//             } elseif (!isset($weekNumber)) {
-//                 $weekNumber = 2; // Hari setelah 1 sebelum Minggu masuk ke minggu kedua
-//             }
-
-//             // Simpan hasil
-//             $weeks[$day] = [
-//                 'date' => $date,
-//                 'week' => $weekNumber
-//             ];
-//         }
-
-//         // Tampilkan hasil dalam format JSON
-//         header('Content-Type: application/json');
-//         echo json_encode($weeks, JSON_PRETTY_PRINT);
-//     }
-// }
-
-class WeekCalculator extends CI_Controller
-{
-    public function index($year = null, $month = null, $day = null)
->>>>>>> 7c27ea1bc4d3668fecef601333b30ec568da7736
     {
         // Set default bulan, tahun, dan hari jika tidak diberikan
         $year = $year ?? date('Y');
