f3b468208234e9e6b49c9c586e0fd738005469cf
kymrs <muhamadrizkysaputra34@gmail.com>
2026-04-23 13:52:26 +0700

    edit comment

--- application/controllers/Ctz_datadeklarasi.php
+++ application/controllers/Ctz_datadeklarasi.php
@@ -31,10 +31,6 @@ class Ctz_datadeklarasi extends CI_Controller
         );
         $pecahkan = explode('-', $tanggal);
 
-        // variabel pecahkan 0 = tanggal
-        // variabel pecahkan 1 = bulan
-        // variabel pecahkan 2 = tahun
-
         return $pecahkan[2] . ' ' . $bulan[(int)$pecahkan[1]] . ' ' . $pecahkan[0];
     }
 
